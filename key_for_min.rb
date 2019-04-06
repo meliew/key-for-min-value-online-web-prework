22a269d71ceb69fc121e54140a9affe5180071a7
Melissa Williams <melissa@brandmeetscopy.com>
2019-04-06 00:08:04 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,10 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+  name_hash.each do |name, value|
+    min_value = value
+    if value < min_value
+      value = min_value
+    
 
 end
